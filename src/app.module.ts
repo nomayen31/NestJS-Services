825a6a73668327105942cf8532083511a2c87119
nomayen31 <nomyen.ohin@gmail.com>
2025-11-22 15:21:53 +0600

    Product , and fetch by id

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { UserController } from './user/user.controller';
+import { ProductService } from './product/product.service';
+import { ProductController } from './product/product.controller';
 
 
 @Module({
   imports: [AuthModule, BookmarkModule],
-  controllers: [UserController],
+  controllers: [UserController, ProductController],
+  providers: [ProductService],
 })
 export class AppModule {}
---
+++ src/product/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductService {
+    private products = [
+        { id: 1, name: "Mobile", price: 20000 },
+        { id: 2, name: "Watch", price: 2500 },
+        { id: 3, name: "Laptop", price: 365000 },
+    ]
+    getAllProducts() {
+        return this.products
+    }
+    getProductById(id: number) {
+        return this.products.find((p) => p.id === id);
+    }
+
+}
